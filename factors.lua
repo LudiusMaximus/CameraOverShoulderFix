b471e9c668d4d03b96d0f01eb039e9e394902f71
Rob <omghixd@hotmail.com>
2023-04-30 22:20:53 +0200

    Update factors.lua
    Cleaned up and added Renewed Proto Drake and Tundra mammoth

--- factors.lua
+++ factors.lua
@@ -128,31 +128,33 @@ cosFix.hardcodedOffsetFactors = {
 
   ["mountId"] = {
 
-          [6] = 7.74,  -- Brown Horse                  - 2019-12-19
-         [17] = 7.6,   -- Felsteed
-         [19] = 6.0,   -- Dire Wolf
-         [71] = 4.7,   -- Gray Kodo
-         [72] = 4.7,   -- Brown Kodo
-         [76] = 4.3,   -- Black War Kodo
-        [101] = 4.3,   -- Great White Kodo
-        [102] = 4.3,   -- Great Gray Kodo
-        [103] = 4.3,   -- Great Brown Kodo
-        [152] = 6.45,  -- Red Hawkstryder
-        [157] = 6.45,  -- Purple Hawkstryder
-        [158] = 6.45,  -- Blue Hawkstryder
-        [159] = 6.45,  -- Black Hawkstryder
-        [203] = 5.71,  -- Cenarion War Hippogryph      - 2019-12-19
-        [268] = 2.5,   -- Albino Drake
-        [309] = 4.7,   -- White Kodo
-        [310] = 6.1,   -- Black Wolf
-        [435] = 7.6,   -- Mountain Horse
-        [547] = 5.7,   -- Hearthsteed
-        [651] = 5.6,   -- Warlord's Deathwheel
-        [780] = 5.4,   -- Felsaber
-        [934] = 5.75,  -- Swift Spectral Hippogryph
+          [6] = 7.74, -- Brown Horse - 19.12.2019
+         [17] = 7.6,  -- Felsteed
+         [19] = 6.0,  -- Dire Wolf
+         [71] = 4.7,  -- Gray Kodo
+         [72] = 4.7,  -- Brown Kodo
+         [76] = 4.3,  -- Black War Kodo
+        [101] = 4.3,  -- Great White Kodo
+        [102] = 4.3,  -- Great Gray Kodo
+        [103] = 4.3,  -- Great Brown Kodo
+        [152] = 6.45, -- Red Hawkstryder
+        [157] = 6.45, -- Purple Hawkstryder
+        [158] = 6.45, -- Blue Hawkstryder
+        [159] = 6.45, -- Black Hawkstryder
+        [203] = 5.71, -- Cenarion War Hippogryph - 19.12.2019
+        [268] = 2.5,  -- Albino Drake
+        [284] = 4.7,  -- Traveler's Tundra Mammoth - 30.04.23
+        [309] = 4.7,  -- White Kodo
+        [310] = 6.1,  -- Black Wolf
+        [435] = 7.6,  -- Mountain Horse
+        [547] = 5.7,  -- Hearthsteed
+        [651] = 5.6,  -- Warlord's Deathwheel
+        [780] = 5.4,  -- Felsaber
+        [934] = 5.75, -- Swift Spectral Hippogryph
 
         -- Thousand and above values (Because OCD)
-        [1563] = 4.7,   -- Highland Drake
+        [1563] = 4.7,  -- Highland Drake - 30.04.23
+        [1589] = 4.7,  -- Renewed Proto-Drake - 30.04.23
 
   },
   
